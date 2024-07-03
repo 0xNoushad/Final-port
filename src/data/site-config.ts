b316cb67cf861857b21f0617d918c28e6b506b32
0xNoushad <choudharynoushad46@gmail.com>
2024-07-03 20:40:55 +0530

    new

---
+++ src/data/site-config.ts
@@ -0,0 +1,91 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Noushad',
+    subtitle: 'A FrontEnd Dev Who Is Learning BackEnd',
+    description: 'Personal Portfolio',
+
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Projects',
+            href: '/projects'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Tags',
+            href: '/tags'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+        {
+            text: 'Terms',
+            href: '/terms'
+        },
+
+    ],
+    socialLinks: [
+
+        {
+            text: 'X/Twitter',
+            href: 'https://x.com/0xNoushad'
+        }
+    ],
+
+
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
